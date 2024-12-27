67cf1bd486e7e38779fe6116ac3da97e4787e42e
mousaibrah <mousa.i@wtm.llc>
2024-12-27 15:09:24 +0300

    Update i18n error and implement tailwind css

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,17 +2,17 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { useTranslation } from 'react-i18next';
+import { Button } from './components/ui/button';
+import i18n from './localization/i18n';
 
 function App() {
-  const { i18n } = useTranslation();
   const [count, setCount] = useState(0);
   useEffect(() => {
     document.documentElement.dir = i18n.dir();
   }, [i18n]);
   return (
-    <>
-      <div>
+    <div className="App flex flex-col justify-center items-center gap-4">
+      <div className="flex flex-row justify-center items-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -21,10 +21,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card flex gap-4 flex-col">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -32,7 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
---
+++ template/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '400px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      },
+      colors: {
+        primary: '',
+        secondary: '',
+        disabled: '',
+        error: ''
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+  variants: {
+    extend: {
+      display: ['ltr', 'rtl'],
+    },
+  },
+};
