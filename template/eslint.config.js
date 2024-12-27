b44444981bd7d7a7d9bb37e559481c778d52534a
mousaibrah <mousa.i@wtm.llc>
2024-12-27 15:26:18 +0300

    improve and take advantage of some of the vscode settings

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +18,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +27,27 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
-  },
-)
+  }
+);
--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
+
 import reactLogo from './assets/react.svg';
+
 import viteLogo from '/vite.svg';
+
 import './App.css';
 import { Button } from './components/ui/button';
 import i18n from './localization/i18n';
 
 function App() {
   const [count, setCount] = useState(0);
+
   useEffect(() => {
     document.documentElement.dir = i18n.dir();
-  }, [i18n]);
+  }, []);
   return (
     <div className="App flex flex-col justify-center items-center gap-4">
       <div className="flex flex-row justify-center items-center">
@@ -22,16 +26,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card flex gap-4 flex-col">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
